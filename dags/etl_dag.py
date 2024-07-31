49496ee4680757a167df632e58fe15b791ca6b73
joaopdrm <joaopedror.marcelino@gmail.com>
2024-07-31 15:55:14 -0300

    first commit

---
+++ dags/etl_dag.py
@@ -0,0 +1,43 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+from scripts.extract import extract_data
+from scripts.transform import transform_data
+from scripts.load import load_data
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'etl_pipeline',
+    default_args=default_args,
+    description='A simple ETL pipeline',
+    schedule_interval=timedelta(days=1),
+)
+
+extract_task = PythonOperator(
+    task_id='extract',
+    python_callable=extract_data,
+    dag=dag,
+)
+
+transform_task = PythonOperator(
+    task_id='transform',
+    python_callable=transform_data,
+    dag=dag,
+)
+
+load_task = PythonOperator(
+    task_id='load',
+    python_callable=load_data,
+    dag=dag,
+)
+
+extract_task >> transform_task >> load_task
